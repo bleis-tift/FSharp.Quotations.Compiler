3f410567e3cb372fcd57a2f997adebd7d6204369
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-19 19:18:14 +0900

    add TestUtil.fs

---
+++ src/FSharp.Quotations.Compiler/ExprCompiler.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharp.Quotations.Compiler
+
+open Microsoft.FSharp.Quotations
+
+module ExprCompiler =
+
+  let compile (expr: Expr<'T>) : 'T =
+    failwith "not impl."
---
+++ tests/FSharp.Quotations.Compiler.Tests/TestUtil.fs
@@ -0,0 +1,19 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Microsoft.FSharp.Quotations
+open FSharp.Quotations.Compiler
+
+[<AutoOpen>]
+module TestUtil =
+  let check (expected: 'T) (expr: Expr<'T>) =
+    expr |> ExprCompiler.compile
+    |> should equal expected
+
+  let checkExn<'TExn when 'TExn :> exn> (expr: Expr<_>) =
+    try
+      ExprCompiler.compile expr |> ignore
+      Assert.Fail("exception is not thrown.")
+    with :? 'TExn -> ()
