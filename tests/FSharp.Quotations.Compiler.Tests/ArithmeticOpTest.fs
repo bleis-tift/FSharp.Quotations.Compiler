56ca26b7d69973fa2b59c69f5e9ab758fee12c8c
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-19 22:33:59 +0900

    add test for checked int + int

--- tests/FSharp.Quotations.Compiler.Tests/ArithmeticOpTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/ArithmeticOpTest.fs
@@ -36,3 +36,11 @@ module ArithmeticOpTest =
   let ``int % int``() =
     <@ 5 % 2 @> |> check 1
     <@ Int32.MinValue % -1 @> |> checkExn<int, OverflowException>
+
+  module Checked =
+    open Microsoft.FSharp.Core.Operators.Checked
+
+    [<Test>]
+    let ``int + int`` () =
+      <@ 1 + 2 @> |> check 3
+      <@ Int32.MaxValue + 1 @> |> checkExn<_, OverflowException>
