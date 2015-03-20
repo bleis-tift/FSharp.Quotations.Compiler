59a3eaf133b167b84ff9da856549176858511154
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-20 11:57:33 +0900

    add tests for cmp bool ops

--- tests/FSharp.Quotations.Compiler.Tests/CmpOpTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/CmpOpTest.fs
@@ -37,3 +37,37 @@ module CmpOpTest =
     <@ 1 >= 0 @> |> check true
     <@ 1 >= 1 @> |> check true
     <@ 0 >= 1 @> |> check false
+
+  [<Test>]
+  let ``bool = bool`` () =
+    <@ true = true @> |> check true
+    <@ false = true @> |> check false
+
+  [<Test>]
+  let ``bool <> bool`` () =
+    <@ true <> true @> |> check false
+    <@ false <> true @> |> check true
+
+  [<Test>]
+  let ``bool < bool`` () =
+    <@ true < false @> |> check false
+    <@ true < true @> |> check false
+    <@ false < true @> |> check true
+
+  [<Test>]
+  let ``bool <= bool`` () =
+    <@ true <= false @> |> check false
+    <@ true <= true @> |> check true
+    <@ false <= true @> |> check true
+
+  [<Test>]
+  let ``bool > bool`` () =
+    <@ true > false @> |> check true
+    <@ true > true @> |> check false
+    <@ false > true @> |> check false
+
+  [<Test>]
+  let ``bool >= bool`` () =
+    <@ true >= false @> |> check true
+    <@ true >= true @> |> check true
+    <@ false >= true @> |> check false
