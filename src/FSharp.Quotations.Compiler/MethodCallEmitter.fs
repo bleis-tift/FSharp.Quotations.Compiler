b2585a8999a7936eeb3bf2795eabf87002ee7885
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-20 09:31:40 +0900

    [impl] compile sbyte int

--- src/FSharp.Quotations.Compiler/MethodCallEmitter.fs
+++ src/FSharp.Quotations.Compiler/MethodCallEmitter.fs
@@ -37,6 +37,7 @@ module internal MethodCallEmitter =
     dict.Add(getMethod <@ 1 <<< 1 @>, emitOneOpCode OpCodes.Shl)
     dict.Add(getMethod <@ ~~~1 @>, emitOneOpCode OpCodes.Not)
     dict.Add(getMethod <@ byte 1 @>, doNothing)
+    dict.Add(getMethod <@ sbyte 1 @>, doNothing)
     dict :> IReadOnlyDictionary<_, _>
 
   open Microsoft.FSharp.Core.Operators.Checked
--- tests/FSharp.Quotations.Compiler.Tests/ConvertFuncTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/ConvertFuncTest.fs
@@ -13,3 +13,13 @@ module ConvertFuncTest =
     // Byte.MinValue = 0uy
     <@ byte -1 @> |> check 255uy
     <@ byte -2 @> |> check 254uy
+
+  [<Test>]
+  let ``sbyte int`` () =
+    <@ sbyte 1 @> |> check 1y
+    // SByte.MaxValue = 127y
+    <@ sbyte 128 @> |> check -128y
+    <@ sbyte 129 @> |> check -127y
+    // SByte.MinValue = -128y
+    <@ sbyte -129 @> |> check 127y
+    <@ sbyte -130 @> |> check 126y
