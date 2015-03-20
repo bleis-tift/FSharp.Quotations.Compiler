1cafc7603f9bf0825b8a46c791de785c0da08343
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-20 09:18:24 +0900

    add tests for cmp int ops

---
+++ tests/FSharp.Quotations.Compiler.Tests/CmpOpTest.fs
@@ -0,0 +1,39 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+open System
+
+[<TestModule>]
+module CmpOpTest =
+  [<Test>]
+  let ``int = int`` () =
+    <@ 1 = 1 @> |> check true
+    <@ 0 = 1 @> |> check false
+
+  [<Test>]
+  let ``int <> int`` () =
+    <@ 1 <> 1 @> |> check false
+    <@ 0 <> 1 @> |> check true
+
+  [<Test>]
+  let ``int < int`` () =
+    <@ 1 < 0 @> |> check false
+    <@ 1 < 1 @> |> check false
+    <@ 0 < 1 @> |> check true
+
+  [<Test>]
+  let ``int <= int`` () =
+    <@ 1 <= 0 @> |> check false
+    <@ 1 <= 1 @> |> check true
+    <@ 0 <= 1 @> |> check true
+
+  [<Test>]
+  let ``int > int`` () =
+    <@ 1 > 0 @> |> check true
+    <@ 1 > 1 @> |> check false
+    <@ 0 > 1 @> |> check false
+
+  [<Test>]
+  let ``int >= int`` () =
+    <@ 1 >= 0 @> |> check true
+    <@ 1 >= 1 @> |> check true
+    <@ 0 >= 1 @> |> check false
