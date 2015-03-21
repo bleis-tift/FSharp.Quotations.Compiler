97e7e0e2fdc0d78e9f7bc8a004949b3f92b6a53b
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-21 22:03:54 +0900

    add test for String.length

---
+++ tests/FSharp.Quotations.Compiler.Tests/StringFuncTest.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+[<TestModule>]
+module StringFuncTest =
+  [<Test>]
+  let ``String.length`` () =
+    <@ String.length "str" @> |> check 3
+    <@ String.length null @> |> check 0
