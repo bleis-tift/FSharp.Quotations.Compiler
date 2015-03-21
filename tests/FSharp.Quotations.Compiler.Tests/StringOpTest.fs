963a3082a008ccd06681a12d19ae7d6097d48482
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-21 20:39:48 +0900

    add tests for string ^ string

---
+++ tests/FSharp.Quotations.Compiler.Tests/StringOpTest.fs
@@ -0,0 +1,11 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+#nowarn "62"
+
+[<TestModule>]
+module StringOpTest =
+  [<TestCase("aaa", "bbb")>]
+  [<TestCase(null, "bbb")>]
+  [<TestCase("aaa", null)>]
+  [<TestCase(null, null)>]
+  let ``string ^ string`` (x, y) = <@ x ^ y @> |> check (x ^ y)
