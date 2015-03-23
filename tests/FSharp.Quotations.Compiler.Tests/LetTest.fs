ad83456eab2e7fb3f6c331f40e5b015add83bda4
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-23 10:33:25 +0900

    add tests for nested let

--- tests/FSharp.Quotations.Compiler.Tests/LetTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/LetTest.fs
@@ -12,3 +12,20 @@ module LetTest =
        n <- 0
        n @>
     |> check 0
+
+  [<Test>]
+  let ``nested let`` () =
+    <@ let a = 10
+       let b = a * 2
+       let a = b + 2
+       a @>
+    |> check 22
+
+  [<Test>]
+  let ``more nested let`` () =
+    <@ let a = 10
+       let b =
+         let a = a * 2
+         a + 2
+       a + b @>
+    |> check 32
