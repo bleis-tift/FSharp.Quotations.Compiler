284fb5193f3802b5805466bbf3de5ac25171170f
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-23 21:13:06 +0900

    add test for property get

--- tests/FSharp.Quotations.Compiler.Tests/ObjTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/ObjTest.fs
@@ -3,7 +3,7 @@
 [<TestModule>]
 module ObjTest =
   type Class (value: int) =
-    member private __.Value = value
+    member __.Value = value
     override __.Equals(x) =
       match x with
       | :? Class as other -> value = other.Value
@@ -16,3 +16,6 @@ module ObjTest =
 
   [<Test>]
   let ``ToString()`` () = <@ (Class(42).ToString()) @> |> check (Class(42).ToString())
+
+  [<Test>]
+  let ``property get`` () = <@ (Class(42)).Value @> |> check 42
