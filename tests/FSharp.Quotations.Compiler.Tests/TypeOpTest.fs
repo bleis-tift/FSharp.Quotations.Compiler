bc671127b041d2c0761ceb2eeabf8549740f2c2f
T.Yamazaki <zyth.asd@gmail.com>
2015-03-27 00:07:47 +0900

    add tests for char type op

--- tests/FSharp.Quotations.Compiler.Tests/TypeOpTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/TypeOpTest.fs
@@ -12,9 +12,16 @@ module TypeOpTest =
   let ``string :> obj`` () = <@ "hoge" :> obj @> |> check ("hoge" :> obj)
 
   [<Test>]
+  let `` char :> obj`` () = <@ 'c' :> obj @> |> check ('c' :> obj)
+
+  [<Test>]
   let ``int :> IEquatable<int>`` () =
     <@ 42 :> IEquatable<int> @> |> check (42 :> IEquatable<int>)
 
   [<Test>]
   let ``string :> IEquatable<string>`` () =
     <@ "hoge" :> IEquatable<string> @> |> check ("hoge" :> IEquatable<string>)
+
+  [<Test>]
+  let ``char :> IEquatable<char>`` () =
+    <@ 'c' :> IEquatable<char> @> |> check ('c' :> IEquatable<char>)
