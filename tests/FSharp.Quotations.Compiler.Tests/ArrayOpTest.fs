2080332abb7d9767ce07cca9032bcbdee6ba132f
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-27 14:29:03 +0900

    add tests for array ops

---
+++ tests/FSharp.Quotations.Compiler.Tests/ArrayOpTest.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+open System
+
+[<TestModule>]
+module ArrayOpTest =
+  [<Test>]
+  let ``get elem from int[]`` () =
+    <@ let xs = [|0..10|] in xs.[5] @> |> check 5
+    <@ let xs = [|0..10|] in xs.[100] @> |> checkExn<_, IndexOutOfRangeException>
+
+  [<Test>]
+  let ``set elem to int[]`` () =
+    <@ let xs = Array.zeroCreate 10
+       xs.[5] <- 10
+       xs.[5] @>
+    |> check 10
+
+  [<Test>]
+  let ``slice int[]`` () =
+    <@ let xs = [|0..10|] in xs.[2..8] @> |> check [|0..10|].[2..8]
