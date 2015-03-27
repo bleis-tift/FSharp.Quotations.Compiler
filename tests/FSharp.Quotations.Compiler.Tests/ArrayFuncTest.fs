3626f303b7de0cd5c1906e98657f6a77d8a63d5c
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-03-27 14:46:58 +0900

    add tests for array functions

---
+++ tests/FSharp.Quotations.Compiler.Tests/ArrayFuncTest.fs
@@ -0,0 +1,32 @@
+﻿namespace FSharp.Quotations.Compiler.Tests
+
+[<TestModule>]
+module ArrayFuncTest =
+  [<Test>]
+  let ``Array.empty`` () =
+    <@ Array.empty: int[] @> |> check [||]
+    <@ Array.empty: string[] @> |> check [||]
+
+  [<Test>]
+  let ``Array.init`` () =
+    <@ Array.init 3 id @> |> check [|0; 1; 2|]
+    <@ Array.init 3 string @> |> check [|"0"; "1"; "2"|]
+
+  [<Test>]
+  let ``Array.create`` () =
+    <@ Array.create 3 1 @> |> check [|1; 1; 1|]
+    <@ Array.create 3 "str" @> |> check [|"str"; "str"; "str"|]
+
+  [<Test>]
+  let ``Array.zeroCreate`` () =
+    <@ Array.zeroCreate 3 @> |> check [|0; 0; 0|]
+    <@ Array.zeroCreate 3 @> |> check [|null; null; null|]
+
+  [<Test>]
+  let ``Array.length`` () = <@ Array.length [|0; 1; 2|] @> |> check 3
+
+  [<Test>]
+  let ``Array.Length`` () = <@ [|0; 1; 2|].Length @> |> check 3
+
+  [<Test>]
+  let ``Array.Map`` () = <@ [|0..10|] |> Array.filter (fun x -> x % 5 = 0) @> |> check [|0; 5; 10|]
