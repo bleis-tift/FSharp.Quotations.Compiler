262b83771a3055e470efc0572b7c1da27d73c5c5
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-04-17 20:12:41 +0900

    add license comments

--- tests/FSharp.Quotations.Compiler.Tests/SeqTest.fs
+++ tests/FSharp.Quotations.Compiler.Tests/SeqTest.fs
@@ -1,4 +1,16 @@
-﻿namespace FSharp.Quotations.Compiler.Tests
+﻿(*
+ * FSharp.Quotations.Compiler - a compiler for F# expression tree
+ * Written in 2015 by bleis-tift (hey_c_est_la_vie@hotmail.co.jp)
+ * kyonmm, zakky-dev
+ * 
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain worldwide.
+ * This software is distributed without any warranty.
+ * 
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software.
+ * If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *)
+namespace FSharp.Quotations.Compiler.Tests
 
 module SeqTest =
   [<Test>]
