93f8c50c5bb5cd7c164960646380320b097d48fe
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-04-19 14:40:52 +0900

    add japanese docs

---
+++ docs/content/ja/limitations.fsx
@@ -0,0 +1,147 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../../bin"
+#I "../../../bin/FSharp.Quotations.Compiler"
+
+#r "FSharp.Quotations.Compiler.dll"
+
+open Microsoft.FSharp.Quotations
+open FSharp.Quotations.Compiler
+
+(**
+制限事項
+========
+このライブラリにはいくつかの制限事項があります。
+
+未実装機能
+----------
+* `ForIntegerRangeLoop`
+* `LetRecursive`
+* `NewDelegate`
+* `WhileLoop`
+
+未サポート機能
+--------------
+* `AddressOf`
+* `AddressSet`
+* `Quote`
+
+技術的な問題
+------------
+### インライン関数
+`NoDynamicInvocationAttribute`付きの他のインライン関数を含むインライン関数は起動できません。
+例えば、次のコードは`System.NotSupportedException`を投げます。
+*)
+
+// (-)はNoDynamicInvocationAttribute付き
+let inline f1 x y = x - y
+try
+  let expr = <@ f1 20 10 @>
+  expr.Execute() |> ignore
+with
+  :? System.NotSupportedException -> printfn "raised exception."
+
+(**
+もちろん、`NoDynamicInvocationAttribute`付きの関数もこのライブラリでは実行できません。
+*)
+
+[<NoDynamicInvocationAttribute >]
+let inline f2 x = x
+try
+  let expr = <@ f2 10 @>
+  expr.Execute() |> ignore
+with
+  :? System.NotSupportedException -> printfn "raised exception."
+
+(**
+もし`NoDynamicInvocationAttribute`付きの関数を実行したい場合、
+`inline`を取るか、手動でインライン化する必要があります。
+*)
+
+let expr = <@ 20 - 10 @>
+printfn "20 - 10 = %d" (expr.Execute())
+
+(**
+他にも、メンバー制約起動式を含むインライン関数をこのライブラリで実行することはできません。
+これに関する回避策はありません。
+
+### mutableとtry-with/try-finally
+`try-with`と`try-finally`は値を持つ式なので、このライブラリではこれらをラムダ式にラップします。
+*)
+
+let tryWithExpr =
+  <@ let x = 10
+     let res =
+       try x with _ -> 20
+     res * 2 @>
+let compiledTryWithExpr = tryWithExpr.Compile()
+
+(**
+これは以下のようにコンパイルされます。
+*)
+
+type lambda0(x) =
+  inherit FSharpFunc<unit, int>()
+
+  override __.Invoke(_) =
+    try x with _ -> 20
+
+let x = 10
+let res = lambda0(x).Invoke()
+res * 2
+
+(**
+`x`はラムダを表すクラスのフィールドとしてコンパイルされます。
+そのため、ラムダ式の本体で`x`を書き換えても、ラムダ式の外側には影響を与えることが出来ません。
+*)
+
+let letMutableAndTryWithExpr =
+  <@ let mutable y = 0
+     let res2 =
+       try y <- 10; y with _ -> 20
+     (y, res2) @>
+let compiledLetMutableAndTryWithExpr = letMutableAndTryWithExpr.Compile()
+
+(**
+これは、以下のようにコンパイルされます。
+*)
+
+type lambda1(y) =
+  inherit FSharpFunc<unit, int>()
+
+  member val y = y with get, set
+  override this.Invoke(_) =
+    try this.y <- 10; y with _ -> 20
+
+let mutable y = 0
+let tmp = lambda1(y)
+let res2 = tmp.Invoke()
+// ラムダ式の起動後に代入すべきだが、現在の実装ではそうなっていない。
+// y <- tmp.y
+(y, res2)
+
+(**
+### Expr.Value
+リテラルを持たない型の`Expr.Value`はサポートされていません。
+*)
+
+let valueExpr: Expr<System.DateTime> =
+  Expr.Value(System.DateTime.UtcNow)
+  |> Expr.Cast
+
+try
+  valueExpr.Execute() |> ignore
+with
+  e -> printfn "%A" e
+
+(**
+`Expr.Value`の代わりに、引用式を使ってください。
+*)
+
+let codeQuote = <@ System.DateTime.UtcNow @>
+printfn "%A" (codeQuote.Execute())
+
+(**
+上の引用式は`Expr.Value`にならず、`Expr.PropertyGet`になります。
+*)
---
+++ docs/content/ja/tutorial.fsx
@@ -0,0 +1,69 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use 
+// it to define helpers that you do not want to show in the documentation.
+#I "../../../bin"
+#I "../../../bin/FSharp.Quotations.Compiler"
+
+#r "FSharp.Quotations.Compiler.dll"
+
+(**
+チュートリアル
+==============
+このライブラリは簡単に使えます。
+
+このライブラリを使うためには、`FSharp.Quotations.Compiler`名前空間をオープンしておくといいでしょう。
+*)
+open FSharp.Quotations.Compiler
+
+(**
+型付きの式木
+------------
+次のような式木を実行したい場合を考えてみます。
+*)
+
+let x = 10
+let y = 20
+let expr = <@ x + y @>
+
+(**
+`Execute`メソッドを使うだけでできます。
+*)
+
+printfn "%d + %d = %d" x y (expr.Execute())
+
+(**
+このコードは`10 + 20 = 30`と表示します。
+
+もし式木をコンパイルして、あとで実行したい場合は、`Compile`メソッドを使います。
+*)
+
+let result = expr.Compile()
+
+(**
+`Compile`メソッドを使うのと同じ意味で`ExprCompiler.compile`関数も使えます。
+*)
+
+let result2 = expr |> ExprCompiler.compile
+
+(**
+`result`を実行するためには、`ExecuteCompiledCode`メソッドを使います。
+*)
+
+printfn "%d + %d = %d" x y (result.ExecuteCompiledCode())
+
+(**
+型なしの式木
+------------
+このライブラリは、型なしの式木を直接はサポートしていません。
+もし型なしの式木を実行したい場合は、`Expr.Cast`メソッドを使います。
+このメソッドはF#の標準ライブラリのメソッドです。
+*)
+
+open Microsoft.FSharp.Quotations
+
+// キャストのためのヘルパ関数
+let cast<'T> (expr: Expr) : Expr<'T> = expr |> Expr.Cast
+
+let untypedExpr = <@@ x + y @@>
+let casted = untypedExpr |> cast<int>
+printfn "%d + %d = %d" x y (casted.Execute())
