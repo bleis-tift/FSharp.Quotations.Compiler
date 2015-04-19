8d56f7996a90f0d4259dc5e8c0c0127b8c59e8aa
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-04-19 17:44:40 +0900

    Bump version to 0.2

--- src/FSharp.Quotations.Compiler/AssemblyInfo.fs
+++ src/FSharp.Quotations.Compiler/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Quotations.Compiler")>]
 [<assembly: AssemblyProductAttribute("FSharp.Quotations.Compiler")>]
 [<assembly: AssemblyDescriptionAttribute("A compiler for F# expression tree")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
+[<assembly: AssemblyVersionAttribute("0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1"
+    let [<Literal>] Version = "0.2"
